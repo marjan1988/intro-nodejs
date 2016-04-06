9684a8bfdaafa3ea9c708a1a14afc3fe7a311196
Marjan Maric <marjan.maric1988@gmail.com>
2016-04-06 13:44:10 +0200

    Created PUBLIC folder with website files and install MIDDLEWARE(bodyParser)

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 //NODE JS module
 var express = require('express');
+//Reguire Middlewares
+var bodyParser = require('body-parser');
 
 //Server app
 var app = express();
 
 exports.start = function(){
+	
+	//MIDDLEWARE
+	app.use(bodyParser.urlencoded());
+	app.use(bodyParser.json());
 	
 	console.log('Server started');
 	
